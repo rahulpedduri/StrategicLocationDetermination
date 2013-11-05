a43e1fadfd64ed8d39e22817acf14a06c9efc1bd
Phani Rahul Pedduri <rahul.pedduri@gmail.com>
2013-11-05 18:29:21 -0500

    Few classes added

---
+++ src/java/util/Census.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.util.ArrayList;
+import beans.State;
+import java.util.List;
+
+
+/**
+ *
+ * @author Phani Rahul
+ */
+public class Census {
+    
+    public static List getStateCodes(){        
+        return null;
+    }
+    public static List getPlaceObjects(State state){
+        return null;
+    }
+    public static List getPlaces(State state){
+        return null;
+    }
+}
