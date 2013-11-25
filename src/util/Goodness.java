2e39aade555d597b44db0b563c85e699c42c0098
Phani Rahul Pedduri <rahul.pedduri@gmail.com>
2013-11-24 22:58:05 -0500

    my goodness

---
+++ src/util/Goodness.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author Phani Rahul
+ */
+public class Goodness {
+    public static double computeCustomerIndicator(){
+        return 0;
+    }
+    public static double computeCompetitorIndicator(){
+        return 0;
+    }
+    public static double computeRealEstateIndicator(){
+        return 0;
+    }
+}
